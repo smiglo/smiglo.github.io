9cb3feb2d5e22c53b67570f68c05bf280f3582dc
Tomasz <tomek.blaszczak@gmail.com>
2025-02-28 23:16:58 +0100

    ios-pwd: clipboard support

--- js/ios-pwd.js
+++ js/ios-pwd.js
@@ -1,102 +1,132 @@
 function getRangeRandom(len) {
-  return Math.floor(Math.random() * len)
+  return Math.floor(Math.random() * len);
 }
 
 function getItem(list) {
-  return list[getRangeRandom(list.length)]
+  return list[getRangeRandom(list.length)];
 }
 
 function getPwd(segments, seglen) {
-  const Digits     = "0123456789"
-  const Vovels     = 'aeiouy'
-  const Consonants = 'bcdfghjklmnpqrstvwz'
+  const Digits = "0123456789";
+  const Vovels = "aeiouy";
+  const Consonants = "bcdfghjklmnpqrstvwz";
 
   const States = {
     Letter: "letter",
-    Conso:  "consonant",
-    Vovel:  "vovel",
-    Digit:  "digit"
-  }
+    Conso: "consonant",
+    Vovel: "vovel",
+    Digit: "digit",
+  };
 
-  let len = segments * seglen
-  let idxDigit = getRangeRandom(len)
-  let idxUpper = (idxDigit + 1 + getRangeRandom(len - 1)) % len
-  let idx = 0
-  let out = ""
+  let len = segments * seglen;
+  let idxDigit = getRangeRandom(len);
+  let idxUpper = (idxDigit + 1 + getRangeRandom(len - 1)) % len;
+  let idx = 0;
+  let out = "";
 
   for (s = 0; s < segments; s++) {
-    let next = States.Letter
+    let next = States.Letter;
     for (l = 0; l < seglen; l++, idx++) {
-      if (idx === idxDigit)
-        next = States.Digit
+      if (idx === idxDigit) next = States.Digit;
+      let c;
       switch (next) {
-      case States.Letter:
-        c = getItem(Vovels + Consonants)
-        if (Vovels.includes(c))
-          next = States.Conso
-        else
-          next = States.Vovel
-        break
-      case States.Conso:
-        c = getItem(Consonants)
-        next = States.Letter
-        break
-      case States.Vovel:
-        c = getItem(Vovels)
-        next = States.Conso
-        break
-      case States.Digit:
-        c = getItem(Digits)
-        next = States.Letter
-        break
-      default:
-        break
+        case States.Letter:
+          c = getItem(Vovels + Consonants);
+          if (Vovels.includes(c)) next = States.Conso;
+          else next = States.Vovel;
+          break;
+        case States.Conso:
+          c = getItem(Consonants);
+          next = States.Letter;
+          break;
+        case States.Vovel:
+          c = getItem(Vovels);
+          next = States.Conso;
+          break;
+        case States.Digit:
+          c = getItem(Digits);
+          next = States.Letter;
+          break;
+        default:
+          break;
       }
-      if (idx === idxUpper)
-        c = c.toUpperCase()
-      out += c
+      if (idx === idxUpper) c = c.toUpperCase();
+      out += c;
     }
-    if (s < segments - 1)
-      out += "-"
+    if (s < segments - 1) out += "-";
   }
-  return out
+  return out;
 }
 
-var segments = 3
-var seglen   = 6
-var pwdCount = 5
+let segments = 3;
+let seglen = 6;
+let pwdCount = 5;
 
-var inCLI = false
-var isHTML = true
-if (typeof process !== 'undefined' && process.release.name === 'node')
-  inCLI=true
-else if (typeof document !== 'undefined')
-  inHTML = true
+let inCLI = false;
+if (typeof process !== "undefined" && process.release.name === "node")
+  inCLI = true;
 
-if (inCLI) {
-  for (i = 0; i <process.argv.length; i++) {
+if (!inCLI) {
+  const urlParams = new URLSearchParams(window.location.search);
+  segments =
+    (urlParams.has("segments") && urlParams.get("segments")) ||
+    (urlParams.has("s") && urlParams.get("s")) ||
+    segments;
+  seglen =
+    (urlParams.has("len") && urlParams.get("len")) ||
+    (urlParams.has("l") && urlParams.get("l")) ||
+    seglen;
+  pwdCount =
+    (urlParams.has("count") && urlParams.get("count")) ||
+    (urlParams.has("c") && urlParams.get("c")) ||
+    pwdCount;
+} else {
+  for (i = 0; i < process.argv.length; i++) {
     switch (process.argv[i]) {
-    case '--': break
-    case '-s': segments = process.argv[++i]; break
-    case '-l': seglen   = process.argv[++i]; break
-    case '-c': pwdCount = process.argv[++i]; break
+      case "--":
+        break;
+      case "-s":
+        segments = process.argv[++i];
+        break;
+      case "-l":
+        seglen = process.argv[++i];
+        break;
+      case "-c":
+        pwdCount = process.argv[++i];
+        break;
     }
   }
-} else if (inHTML) {
-  const urlParams = new URLSearchParams(window.location.search)
-  segments = urlParams.has('segments') && urlParams.get('segments') || urlParams.has('s') && urlParams.get('s') || segments
-  seglen = urlParams.has('len') && urlParams.get('len') || urlParams.has('l') && urlParams.get('l') || seglen
-  pwdCount = urlParams.has('count') && urlParams.get('count') || urlParams.has('c') && urlParams.get('c') || pwdCount
 }
 
-if (inHTML)
-  document.body.innerHTML += "<ul>"
-for (i = 0; i < pwdCount; i++) {
-  let out = getPwd(segments, seglen)
-  console.log(out)
-  if (inHTML)
-    document.body.innerHTML += "<li>" + out + "</li>"
+if (!inCLI) {
+  document.body.innerHTML += "<ul id='pwd-list'></ul>";
+  const pwdList = document.getElementById("pwd-list");
+  for (i = 0; i < pwdCount; i++) {
+    let out = getPwd(segments, seglen);
+    console.log(out);
+    const listItem = document.createElement("li");
+    listItem.addEventListener("click", () => {
+      navigator.clipboard
+        .writeText(listItem.textContent)
+        .then(() => {
+          console.log("Copied to clipboard:", listItem.textContent);
+          listItem.style.backgroundColor = "lightgreen";
+          listItem.style.color = "black";
+          setTimeout(() => {
+            listItem.style.backgroundColor = "";
+            listItem.style.color = "";
+          }, 750);
+        })
+        .catch((err) => {
+          console.error("Failed to copy:", err);
+        });
+    });
+    pwdList.appendChild(listItem);
+  }
+} else {
+  for (i = 0; i < pwdCount; i++) {
+    let out = getPwd(segments, seglen)
+    console.log(out)
+  }
 }
-if (inHTML)
-  document.body.innerHTML += "</ul>"
 
