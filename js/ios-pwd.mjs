ffa8364ca60277217f7efcb2232504c7bf9568c8
Tomasz <tomek.blaszczak@gmail.com>
2025-03-11 22:56:39 +0100

    ios-pwd: refactor, p2

--- js/ios-pwd.mjs
+++ js/ios-pwd.mjs
@@ -8,12 +8,18 @@ function getItem(prng, list) {
   return list[getRangeRandom(prng, list.length)];
 }
 
-function getPwd(prng, segments, seglen) {
-  const Digits = "0123456789";
-  const Vovels = "aeiouy";
-  const Consonants = "bcdfghjklmnpqrstvwz";
+function checkValue(value, defValue) {
+  let v = parseInt(value);
+  v = isNaN(v) ? defValue : v;
+  return v;
+}
 
-  const States = {
+function getPwd(prng, segments, seglen) {
+  const DIGITS = "0123456789";
+  const VOVELS = "aeiouy";
+  const CONSONANTS = "bcdfghjklmnpqrstvwz";
+
+  const STATES = {
     Letter: "letter",
     Conso: "consonant",
     Vovel: "vovel",
@@ -24,72 +30,72 @@ function getPwd(prng, segments, seglen) {
   let idxDigit = getRangeRandom(prng, len);
   let idxUpper = (idxDigit + 1 + getRangeRandom(prng, len - 1)) % len;
   let idx = 0;
-  let out = "";
+  let out = [];
 
   for (let s = 0; s < segments; s++) {
-    let next = States.Letter;
+    let next = STATES.Letter;
     for (let l = 0; l < seglen; l++, idx++) {
-      if (idx === idxDigit) next = States.Digit;
+      if (idx === idxDigit) next = STATES.Digit;
       let c;
       switch (next) {
-        case States.Letter:
-          c = getItem(prng, Vovels + Consonants);
-          if (Vovels.includes(c)) next = States.Conso;
-          else next = States.Vovel;
+        case STATES.Letter:
+          c = getItem(prng, VOVELS + CONSONANTS);
+          if (VOVELS.includes(c)) next = STATES.Conso;
+          else next = STATES.Vovel;
           break;
-        case States.Conso:
-          c = getItem(prng, Consonants);
-          next = States.Letter;
+        case STATES.Conso:
+          c = getItem(prng, CONSONANTS);
+          next = STATES.Letter;
           break;
-        case States.Vovel:
-          c = getItem(prng, Vovels);
-          next = States.Conso;
+        case STATES.Vovel:
+          c = getItem(prng, VOVELS);
+          next = STATES.Conso;
           break;
-        case States.Digit:
-          c = getItem(prng, Digits);
-          next = States.Letter;
+        case STATES.Digit:
+          c = getItem(prng, DIGITS);
+          next = STATES.Letter;
           break;
         default:
           break;
       }
       if (idx === idxUpper) c = c.toUpperCase();
-      out += c;
+      out.push(c);
     }
-    if (s < segments - 1) out += "-";
+    if (s < segments - 1) out.push("-");
   }
-  return out;
+  return out.join("");
+}
+
+function createPasswordItem(password, index) {
+    const listItem = document.createElement("li");
+    const info = document.createElement("span");
+    info.textContent = index + ": ";
+
+    const itemContent = document.createElement("span");
+    itemContent.textContent = password;
+    itemContent.classList.add('pwd-item');
+
+    listItem.appendChild(info);
+    listItem.appendChild(itemContent);
+    return listItem;
 }
 
 function appendItem(pwdList, password, index) {
-  const listItem = document.createElement("li");
-  const info = document.createElement("spam");
-  info.textContent = index + ": ";
-  const itemContent = document.createElement("span");
-  itemContent.textContent = password;
-
-  itemContent.style.padding = "0.2rem";
-  itemContent.style.display = "inline-block";
-  itemContent.style.transition = "background-color 150ms ease";
-
+  const listItem = createPasswordItem(password, index);
+  const itemContent = listItem.querySelector('span.pwd-item');
   itemContent.addEventListener("click", () => {
     navigator.clipboard
       .writeText(itemContent.textContent)
       .then(() => {
-        itemContent.style.backgroundColor = "lightgreen";
-        itemContent.style.color = "black";
+        itemContent.classList.add("copied");
         setTimeout(() => {
-          itemContent.style.transition = "background-color 350ms ease";
-          itemContent.style.backgroundColor = "";
-          itemContent.style.color = "";
-        }, 150);
+          itemContent.classList.remove("copied");
+        }, 350);
       })
       .catch((err) => {
         console.error("Failed to copy:", itemContent.textContent, err);
       });
   });
-
-  listItem.appendChild(info);
-  listItem.appendChild(itemContent);
   pwdList.appendChild(listItem);
 }
 
@@ -97,14 +103,12 @@ let segments = 3;
 let seglen = 6;
 let idx = undefined;
 let phrase = "default-seed";
-let min = Math.floor(Math.random() * 1000) * 10;
-let max = undefined;
+let min = Math.floor(Math.random() * 1000) * 10 + 1;
+let count = 10;
 
-let inCLI = false;
-if (typeof process !== "undefined" && process.release.name === "node")
-  inCLI = true;
+const IN_CLI = typeof process !== 'undefined' && process.versions?.node;
 
-if (!inCLI) {
+if (!IN_CLI) {
   const urlParams = new URLSearchParams(window.location.search);
   segments =
     (urlParams.has("segments") && urlParams.get("segments")) ||
@@ -119,7 +123,7 @@ if (!inCLI) {
     (urlParams.has("p") && urlParams.get("p")) ||
     phrase;
   min = (urlParams.has("min") && urlParams.get("min")) || min;
-  max = (urlParams.has("max") && urlParams.get("max")) || max;
+  count = (urlParams.has("count") && urlParams.get("count")) || count;
   idx = (urlParams.has("idx") && urlParams.get("idx")) || idx;
 } else {
   for (let i = 0; i < process.argv.length; i++) {
@@ -128,8 +132,10 @@ if (!inCLI) {
       case "-l": seglen = process.argv[++i]; break;
       case "-p":
       case "--phrase": phrase = process.argv[++i]; break;
+      case "-m":
       case "--min": min = process.argv[++i]; break;
-      case "--max": max = process.argv[++i]; break;
+      case "-c":
+      case "--count": count = process.argv[++i]; break;
       case "-i":
       case "-idx": idx = process.argv[++i]; break;
       default:
@@ -138,34 +144,32 @@ if (!inCLI) {
   }
 }
 
-segments = parseInt(segments);
-segments = isNaN(segments) ? 3 : segments;
-seglen = parseInt(seglen);
-seglen = isNaN(seglen) ? 6 : seglen;
-min = parseInt(min);
-min = isNaN(min) ? 0 : min;
-max = parseInt(max);
-max = isNaN(max) ? 10 : max;
-if (min === undefined || min < 0) min = 0;
-if (max === undefined || max <= min) max = min + 10;
-idx = parseInt(idx);
-idx = isNaN(idx) ? undefined : idx;
+segments = checkValue(segments, 3);
+seglen = checkValue(seglen, 6);
+min = checkValue(min, 1);
+count = checkValue(count, 10);
+if (min < 1) min = 1;
+if (count <= 0) count = 10;
+idx = checkValue(idx, undefined);
 if (idx !== undefined) {
-  min = max = idx;
+  min = idx;
+  count = 1;
 }
 
-const pwdCount = max - min + 1;
-const prng = createPRNG(phrase);
+const PRNG_MAIN = createPRNG(phrase);
 
-for (let i = 0; i < min; i++) prng.next();
+console.log(`init: ${segments}x${seglen}, ${min}/${idx}, +${count}`);
+console.log(`seed: ${phrase}, 0x${PRNG_MAIN.seed.toString(16)}/0x${PRNG_MAIN.childSeed.toString(16)}`);
 
-const passwords = Array.from({ length: pwdCount }, () => getPwd(prng.newPrng(), segments, seglen));
+for (let i = 0; i < min; i++) PRNG_MAIN.next();
 
-if (!inCLI) {
+const passwords = Array.from({ length: count }, () => getPwd(PRNG_MAIN.newPrng(), segments, seglen));
+
+if (!IN_CLI) {
   const prngInfo = document.getElementById("prng-info");
   prngInfo.textContent = "?phrase=" + phrase;
   if (idx === undefined) {
-    prngInfo.textContent += "&min=" + min + "&max=" + max;
+    prngInfo.textContent += "&min=" + min + "&count=" + count;
   } else {
     prngInfo.textContent += "&idx=" + idx;
   }
@@ -173,10 +177,5 @@ if (!inCLI) {
   const pwdList = document.getElementById("pwd-list");
   passwords.forEach((pwd, i) => appendItem(pwdList, pwd, min+i));
 } else {
-  if (idx === undefined) {
-    console.log(`${phrase}, [${min}, ${max}]`);
-  } else {
-    console.log(`${phrase}, i=${idx}`);
-  }
   passwords.forEach((pwd, i) => console.log(`${min+i}: ${pwd}`))
 }
